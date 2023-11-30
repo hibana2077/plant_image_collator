0fb7b6014b6bd154e4a2e4a9b442db4c6b3ce4d2
hibana2077 <hibana2077@gmail.com>
2023-11-30 09:49:42 +0800

    Add file header to lab2.py

--- lab2.py
+++ lab2.py
@@ -1,3 +1,11 @@
+'''
+Author: hibana2077 hibana2077@gmail.com
+Date: 2023-11-29 01:05:56
+LastEditors: hibana2077 hibana2077@gmail.com
+LastEditTime: 2023-11-30 09:49:33
+FilePath: \plant_image_collator\lab2.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 import base64
 from PIL import Image
 from io import BytesIO
