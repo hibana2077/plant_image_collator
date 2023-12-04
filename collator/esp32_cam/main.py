10942b1a68f10e1e43750b63231b232ea6bfb801
hibana2077 <hibana2077@gmaill.com>
2023-12-04 14:10:42 +0800

    Delete unnecessary files and update dependencies

---
+++ collator/esp32_cam/main.py
@@ -0,0 +1,18 @@
+'''
+Author: hibana2077 hibana2077@gmaill.com
+Date: 2023-12-04 14:10:02
+LastEditors: hibana2077 hibana2077@gmaill.com
+LastEditTime: 2023-12-04 14:10:36
+FilePath: /plant_image_collator/collator/esp32_cam/main.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+import time
+
+def main():
+    while True:
+        # 在這裡撰寫你的程式邏輯
+        print("Hello, MicroPython!")
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
