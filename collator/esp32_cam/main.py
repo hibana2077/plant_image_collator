daff676ef533bf39fd6ba23811814e88a745fdc0
hibana2077 <hibana2077@gmaill.com>
2023-12-04 14:16:17 +0800

    Update main.py file

--- collator/esp32_cam/main.py
+++ collator/esp32_cam/main.py
@@ -2,7 +2,7 @@
 Author: hibana2077 hibana2077@gmaill.com
 Date: 2023-12-04 14:10:02
 LastEditors: hibana2077 hibana2077@gmaill.com
-LastEditTime: 2023-12-04 14:10:36
+LastEditTime: 2023-12-04 14:16:11
 FilePath: /plant_image_collator/collator/esp32_cam/main.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -10,7 +10,6 @@ import time
 
 def main():
     while True:
-        # 在這裡撰寫你的程式邏輯
         print("Hello, MicroPython!")
         time.sleep(1)
 
